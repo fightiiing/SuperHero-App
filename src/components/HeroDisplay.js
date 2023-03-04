29a6b22f2d673c0e245b5b3361b5ee5407ccc1e8
Mike Giordano <pitiiing@gmail.com>
2023-03-04 00:49:32 -0500

    reduced the hero display code

--- src/components/HeroDisplay.js
+++ src/components/HeroDisplay.js
@@ -1,38 +1,7 @@
 export default function  HeroDisplay ({ hero }){
 
-//the display returned if superman, thing, or batman was searched for based on the conditional below. Different location chosen within the returned json data object.
-  const realSuperhero = () => {
-    return (
-      <div className="heroContainer">
-        <div className="heroStats">
-          <h1>{hero.results[1].name}</h1>
-          <h3>Combat = {hero.results[1].powerstats.combat}</h3>
-          <h3>IQ = {hero.results[1].powerstats.intelligence} <span className="divider">|</span> Strength = {hero.results[1].powerstats.strength} </h3>
-          <h3>Speed = {hero.results[1].powerstats.speed} <span className="divider">|</span> Durability = {hero.results[1].powerstats.durability}</h3>
-          <h3>Height = {hero.results[1].appearance.height[0]} <span className="divider">|</span> Weight = {hero.results[1].appearance.weight[0]}</h3>
-          </div>     
-          <img src={hero.results[1].image.url} alt={hero.results[1].name} />       
-      </div>
-    );  
-};
-
-//the display if thing was searched for
-const thing = () => {
-  return (
-    <div className="heroContainer">
-     <div className="heroStats">
-      <h1>{hero.results[3].name}</h1>
-      <h3>Combat = {hero.results[3].powerstats.combat}</h3>
-      <h3>IQ = {hero.results[3].powerstats.intelligence} <span className="divider">|</span> Strength = {hero.results[3].powerstats.strength} </h3>
-      <h3>Speed = {hero.results[3].powerstats.speed} <span className="divider">|</span> Durability = {hero.results[3].powerstats.durability}</h3>
-      <h3>Height = {hero.results[3].appearance.height[0]} <span className="divider">|</span> Weight = {hero.results[3].appearance.weight[0]}</h3>
-      </div>     
-      <img src={hero.results[3].image.url} alt={hero.results[3].name} />       
-  </div>
-  );  
-};
-
-const random = () => {
+//how data searched via character id is displayed 
+const numeric = () => {
   return (
     <div className="heroContainer">
      <div className="heroStats">
@@ -47,7 +16,7 @@ const random = () => {
   );  
 };
 
-//the standard data picked to display for returned superhero data
+//how data searched via text is displayed 
   const loaded = () => {
       return (
         <div className="heroContainer">
@@ -63,28 +32,14 @@ const random = () => {
       );
     };
   
-//function to return loading JSX, if data isn't available
     const loading = () => {
       return <h1>No Superhero to Display</h1>;
     };
 
-//conditional which chooses which function to run, which determines where the data is to be pulled from, depending on the superhero data. 'hero' is the json data passed in.   
+//conditional which chooses which function to run depending on the superhero data.
   if (hero){
       if (hero.id){
-        //random superhero searched by character id, which returns differently formatted JSON data than a string search
-        return random();      
-      } else if (hero.results[0].id==="195" && hero.results[1]){
-        //superman  
-        return realSuperhero();
-      } else if (hero.results[0].id==="69"){
-        //batman
-        return realSuperhero();
-      } else if (hero.results[0].id==="405" && hero.results[1]){
-        //thor
-        return realSuperhero();
-      } else if (hero.results[0].id==="428" && hero.results[3]){
-        //thing
-        return thing();
+        return numeric();      
       } else {
         return loaded();
       }   
